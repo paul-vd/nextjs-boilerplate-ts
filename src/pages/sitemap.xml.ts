b678f86aad5278a55ef2b1059c06861340b7e06f
Paul van Dyk <paul@protocode.io>
2020-04-14 08:33:04 +0200

    contain sitemap props

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,22 +3,29 @@ import { SitemapItem, EnumChangefreq } from "sitemap";
 import { SitemapStream, streamToPromise } from "sitemap";
 import { NextApiRequest, NextApiResponse, NextPageContext } from "next";
 
-interface RouteItem extends Omit<SitemapItem, "img" | "links" | "video"> {
-  title: string; // title to display in components
-  sitemap: boolean; // filter routes when constructing sitemap
-  navigation: boolean | "main" | "footer"; // filter routes when constructing navigation [ true === ("main" && "footer") | false === "none" | "main" || "footer" ]
+interface TransformedSitemapItem
+  extends Omit<SitemapItem, "img" | "links" | "video"> {
   img?: SitemapItem["img"]; // transform type to optional
   links?: SitemapItem["links"]; // transform type to optional
   video?: SitemapItem["video"]; // transform type to optional
 }
 
+interface RouteItem {
+  title: string; // title to display in components
+  path: string;
+  sitemap?: TransformedSitemapItem; // filter routes when constructing sitemap
+  navigation: boolean | "main" | "footer"; // filter routes when constructing navigation [ true === ("main" && "footer") | false === "none" | "main" || "footer" ]
+}
+
 export const routes: RouteItem[] = [
   {
     title: "landing",
-    sitemap: true,
+    path: "/",
+    sitemap: {
+      url: "/",
+      changefreq: EnumChangefreq.YEARLY,
+    },
     navigation: false,
-    url: "/",
-    changefreq: EnumChangefreq.YEARLY,
   },
 ];
 
@@ -32,7 +39,7 @@ class Sitemap extends React.Component {
     const hostname = "https://" + req.headers.host;
     res.setHeader("Content-Type", "text/xml");
     const smStream = new SitemapStream({ hostname });
-    for (const route of routes) smStream.write(route);
+    for (const route of routes) route.sitemap && smStream.write(route.sitemap);
     smStream.end();
     const sitemap = await streamToPromise(smStream).then((sm) => sm.toString());
     res.write(sitemap);
